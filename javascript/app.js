e257f671aad83248c4ef219658f7c17c4ec8dfda
Tiles Master <tilesmaster@Tiless-MacBook-Pro.local>
2017-11-04 12:54:28 -0400

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,71 +1,59 @@
+// $(document).ready(function(){
 
-// var newData = [
-// 	{ //<tr>
-// 		trainName:"Joan",//<td>
-// 		destination:"Off Arc",//<td>
-// 		frequency
-// 		firstTime:0,//<td>
-// 		frequency:0,//<td>
-// 	},
-// 	{ //<tr>
-// 		trainName:"King",//<td>
-// 		destination:"Arthur",//<td>
-// 		firstTime:1,//<td>
-// 		frequency:1,//<td>
-// 	}
-// ]
-// for (var i = 0; i < newData.length; i++) {
-// 	newData[i];
-// 	console.log(newData[i]);
-// }
-// console.log(newData[0].trainName);     
-// console.log(newData[0].destination);     
-// console.log(newData[0].firstTime);     
-// console.log(newData[0].frequency);     
-// console.log(newData[1].name);     
-// Capture Button Click
-$("#submit").on("click", function(event) {
-  event.preventDefault();
+ console.log("working");
+ var config = {
+    apiKey: "AIzaSyCBzzyQfD76zgurrzAvqxxySwS6Rv6czjE",
+    authDomain: "employeem-23e7e.firebaseapp.com",
+    databaseURL: "https://employeem-23e7e.firebaseio.com",
+    projectId: "employeem-23e7e",
+    storageBucket: "employeem-23e7e.appspot.com",
+    messagingSenderId: "382300694090"
+  };
+  firebase.initializeApp(config);
 
-      // Capture User Inputs and store them into variables
-      var trainName = $("#train-name").val().trim();
-      var destination = $("#destination").val().trim();
-      var firstTrainTime = $("#first-train-time").val().trim();
-      var frequency = $("#frequency").val().trim();
 
-      // Console log each of the user inputs to confirm we are receiving them correctly
-      console.log(trainName);
-      console.log(destination);
-      console.log(firstTrainTime);
-      console.log(frequency);
+  // variables
+  var database = firebase.database();
 
-      //loop through the object data and assign each to td's
+  var employeeName = "";
+  var role = "";
+  var startDate = "";
+  var monthlyRate = "";
+  
 
-      //dynamically append new <tr> that contains 5 <td>
-      	var newTR = $("<tr>");
+  //onclick
+  $("#submit").on("click", function (event){
+      event.preventDefault();
 
-      	newTR.text(trainName);
-      	$('.table > tbody:last').append(newTR); 
+      employeeName = $("#employee-name").val().trim();
+      role = $("#role").val().trim();
+      startDate = $("#start-date").val().trim();
+      monthlyRate = $("#monthly-rate").val().trim();
 
-      // Clear absolutely everything stored in localStorage using localStorage.clear()
-      localStorage.clear();
+      console.log(employeeName);
 
-      // Store the username into localStorage using "localStorage.setItem"
-      localStorage.setItem("train-name", trainName);
-      localStorage.setItem("destination", destination);
-      localStorage.setItem("first-train-time", firstTrainTime);
-      localStorage.setItem("frequency", frequency);
+
+      database.ref().push({
+            employeeName:employeeName,
+            role:role,
+            startDate:startDate,
+            monthlyRate:monthlyRate
+      })
+  })
+
+  //pull
+  database.ref().limitToLast(1).on("child_added", function(childSnapshot) {
+      console.log(childSnapshot.val());
+      
+      $("#employee").append(childSnapshot.val().employeeName);
+  })
 
 
 
-      $("#train-name").text(localStorage.getItem("train-name"));
-      $("#destination").text(localStorage.getItem("destination"));
-      $("#first-train-time").text(localStorage.getItem("first-train-time"));
-      $("#frequency").text(localStorage.getItem("frequency"));
-});
 
-	$("#train-name").text(localStorage.getItem("train-name"));
-	$("#destination").text(localStorage.getItem("destination"));
-	$("#first-train-time").text(localStorage.getItem("first-train-time"));
-	$("#frequency").text(localStorage.getItem("frequency"));
-	
+
+
+
+
+
+
